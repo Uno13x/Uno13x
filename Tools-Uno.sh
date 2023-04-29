7a9157e5ab7f09064d32bfb67d5cdb6b231cbaa1
Uno13x <132174647+Uno13x@users.noreply.github.com>
2023-04-29 20:34:51 -0300

    Add files via upload

---
+++ Tools-Uno.sh
@@ -0,0 +1,46 @@
+#!/system/bin/sh
+
+echo TOOLS UNO
+echo ___________________________________
+echo [1] biyivi_ataque_DDOS
+echo [2] IP-Tracker
+echo [3] Zphisher
+echo [4] Slowloris
+echo [5] Persux
+echo [6] Ip-Tracer
+echo [7] Cam-Hackers
+echo [8] NMAP
+echo [9] Hall
+echo [10] Xteam
+echo ___________________________________
+
+echo Extraindo os comandos:
+git clone https://github.com/biyivi/biyivi_ataque_DDos
+  
+pkg update & pkg install nmap
+  
+git clone https://github.com/anonymousproo/IP-Tracker.git
+  
+git clone https://github.com/htr-tech/zphisher
+  
+git clone https://github.com/gkbrk/slowloris
+  
+git clone https://github.com/Lursy7/Persux  
+  
+git clone https://github.com/B3N1N1/TheWatchers                                           
+
+git clone https://github.com/rajkumardusad/IP-Tracer
+  
+git clone https://github.com/AngelSecurityTeam/Cam-Hackers  
+
+git clone https://github.com/xploitstech/Xteam
+  
+                
+                                                          
+
+
+                                                           
+
+
+
+
